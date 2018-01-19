8f22a346848b5a4731b3bba937a635abab471810
奥野雅之 <xm14mx@gmail.com>
2018-01-19 11:49:10 +0900

    update in 0

--- tests/CalcTest.php
+++ tests/CalcTest.php
@@ -12,6 +12,7 @@ class CalcTest extends TestCase
       $this->assertEquals($calc->sub(3,2),1);
       $this->assertEquals($calc->mul(3,2),6);
       $this->assertEquals($calc->div(3,2),1.5);
+      $this->assertEquals($calc->div(3,0),0);
     }
 }
 ?>
